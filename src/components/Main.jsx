ea3222fa4607387d251315856b1a39f32de6a554
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-03-31 17:19:34 +0200

    bootstrap icon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,70 @@
+//import * as Icon from 'react-bootstrap-icons';
 import { Component } from "react";
 import { Container, Button, DropdownButton, Dropdown } from "react-bootstrap";
 
 import MyGallery from "./MyGallery";
 import MyFooter from "./MyFooter";
 
-const API_KEY = "8f194417";
-const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=Harry+Potter`;
+import img1 from '../assets/img/1.png';
+import img2 from '../assets/img/2.png';
+import img3 from '../assets/img/3.png';
+import img4 from '../assets/img/4.png';
+import img5 from '../assets/img/5.png';
+import img6 from '../assets/img/6.png';
+import img7 from '../assets/img/7.png';
+import img8 from '../assets/img/8.png';
+import img9 from '../assets/img/9.png';
+import img10 from '../assets/img/10.png';
+import img11 from '../assets/img/11.png';
+import img12 from '../assets/img/12.png';
+import img13 from '../assets/img/13.png';
+import img14 from '../assets/img/14.png';
+import img15 from '../assets/img/15.png';
+import img16 from '../assets/img/16.png';
+import img17 from '../assets/img/17.png';
+import img18 from '../assets/img/18.png';
 
-class Main extends Component {
-    state = {
-        movies: []
-    };
-    fetchMovies= async () =>{
-        try {
-            console.log(API_URL);
-            const response = await fetch(API_URL);
-      
-            if (response.ok) {
-              const data = await response.json();
-              console.log("FETCH RESULT: ", data);
-              //this.setState({ movies:data });
-              console.log("SET STATE");
-            } 
-          } catch (error) {
-            console.log(error);
-          }
+const movies = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18
+];
+
+function getRandomMovies() {
+    const randomMovies = [];
+    const usedIndexes = [];
+
+    while (randomMovies.length < 6) {
+        const randomIndex = Math.floor(Math.random() * movies.length);
+        if (!usedIndexes.includes(randomIndex)) {
+            usedIndexes.push(randomIndex);
+            randomMovies.push(movies[randomIndex]);
+        }
     }
 
-    componentDidMount = () => {
-        console.log("COMPONENT DID MOUNT");
-        this.fetchMovies();
-      };
-    
+    return randomMovies;
+}
 
+class Main extends Component {
     render() {
-        /*const trendingNowMovies = getRandomMovies();
+        const trendingNowMovies = getRandomMovies();
         const watchItAgainMovies = getRandomMovies();
-        const newReleasesMovies = getRandomMovies();*/
+        const newReleasesMovies = getRandomMovies();
         return (
             <Container fluid style={{ backgroundColor: "#221f1f" }} className="text-light">
                 <div className="d-flex justify-content-between">
@@ -61,26 +89,13 @@ class Main extends Component {
                         </Button>
                     </div>
                 </div>
+                <MyGallery title="Trending Now" movies={trendingNowMovies} />
+                <MyGallery title="Watch it Again" movies={watchItAgainMovies} />
+                <MyGallery title="New Releases" movies={newReleasesMovies} />
                 <MyFooter />
             </Container>
         );
     }
 }
-
-/*function getRandomMovies(movies) {
-    console.log(movies);
-    const randomMovies = [];
-    const usedIndexes = [];
-
-    while (randomMovies.length < 6) {
-        const randomIndex = Math.floor(Math.random() * movies.length);
-        if (!usedIndexes.includes(randomIndex)) {
-            usedIndexes.push(randomIndex);
-            randomMovies.push(movies[randomIndex]);
-        }
-    }
-
-    return randomMovies;
-}*/
 
 export default Main;
--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,69 +1,99 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
 
-
 const MyFooter = () => {
     return (
-        <footer className="text-light" style={{ backgroundColor: "#221f1f" }}>
-            <Container fluid>
-                <Row className="text-center mt-5">
-                    <Col xs={6} className="offset-3">
+        <footer>
+            <Container fluid className="mt-5" style={{ backgroundColor: "#221f1f" }}>
+                <Row className="text-center">
+                    <Col md={{ span: 6, offset: 3 }}>
                         <Row>
                             <Col xs={12} className="text-left mb-2">
-                                <a href="#">Facebook</a>
-                                <a href="#">Instagram</a>
-                                <a href="#">Twitter</a>
-                                <a href="#">YouTube</a>
+                                
                             </Col>
                         </Row>
                         <Row className="row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-4">
                             <Col>
                                 <Row>
-                                    <Col xs={12}>
-                                        <a href="#">Audio and Subtitles</a>
-                                        <a href="#">Media Center</a>
-                                        <a href="#">Privacy</a>
-                                        <a href="#">Contact us</a>
+                                    <Col xs={12} className="text-start my-4 mb-0">
+
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Audio and Subtitles</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Media center</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Privacy</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Contact us</a>
+                                        </p>
                                     </Col>
                                 </Row>
                             </Col>
                             <Col>
                                 <Row>
-                                    <Col xs={12}>
-                                        <a href="#">Audio Description</a>
-                                        <a href="#">Investor Relations</a>
-                                        <a href="#">Legal Notices</a>
+                                    <Col xs={12} className="text-start my-4">
+
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Audio Description</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Investor Relations</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Legal Notices</a>
+                                        </p>
+
                                     </Col>
                                 </Row>
                             </Col>
                             <Col>
                                 <Row>
-                                    <Col xs={12}>
-                                        <a href="#">Help Center</a>
-                                        <a href="#">Jobs</a>
-                                        <a href="#">Cookie Preferences</a>
+                                    <Col xs={12} className="text-start my-4">
+
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Help Center</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Jobs</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Cookie Preferences </a>
+                                        </p>
+
                                     </Col>
                                 </Row>
                             </Col>
                             <Col>
                                 <Row>
-                                    <Col xs={12}>
-                                        <a href="#">Gift Cards</a>
-                                        <a href="#">Terms of Use</a>
-                                        <a href="#">Corporate Information</a>
+                                    <Col xs={12} className="text-start my-4">
+
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Gift Card</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Terms of Use</a>
+                                        </p>
+                                        <p className="m-1" style={{ fontSize: "0.9em" }}>
+                                            <a href="#" alt="footer link" className="text-decoration-none text-light fw-light opacity-75">Corporate Information</a>
+                                        </p>
                                     </Col>
                                 </Row>
                             </Col>
                         </Row>
                         <Row>
-                            <Col xs={12} className="text-left mb-2">
-                                <Button variant="secondary" size="sm" className="rounded-0 mt-3">
+                            <Col xs={12} className="text-start" >
+                                <Button variant="outline-light" className="btn-sm footer-button rounded-0 m-2 ms-0  opacity-75">
                                     Service Code
                                 </Button>
                             </Col>
                         </Row>
                         <Row>
-                            <Col xs={12} className="text-left mb-2 mt-2">
-                                © 1997-2022 Netflix, Inc.
+                            <Col xs={12} className="text-decoration-none text-start text-light fw-light opacity-75" >
+                                <p className="m-1 ms-0 mb-2" style={{ fontSize: "0.9em" }}>
+                                    © 1997-2022 Netflix, Inc.
+                                </p>
                             </Col>
                         </Row>
                     </Col>
